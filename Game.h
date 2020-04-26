883df5ec53d3f335de88893c27cd607dd9523fa1
BIGGESTLION <60803858+BIGGESTLION@users.noreply.github.com>
2020-04-26 12:45:09 +0800

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,92 @@
+#pragma once
+#include<vector>
+using namespace std;
+//坐标结构体
+struct Coor {
+	int x;
+	int y;
+};
+//地图类
+class Map
+{
+private:
+	//初始化地图
+	void init();
+	//绘制界面
+	void draw();
+public:
+	void Init() {
+		init();
+	}
+	void Draw() {
+		draw();
+	}
+	
+};
+class Snack
+{
+private:
+	int len;
+	int speed;
+	vector<Coor> co;
+	void init();
+	void expand();
+	bool check(int x, int y);
+	void run(int dir);
+	bool collision();
+	void faster();
+	void sleep();
+	bool eat(int x, int y);
+public:
+	Snack() = default;
+	int getLen() {
+		return len;
+	}
+	int getSpeed() {
+		return speed;
+	}
+	void Init() {
+		init();
+	}
+	void Expand() {
+		expand();
+		faster();
+	}
+	bool Check(int x, int y) {
+		return check(x, y);
+	}
+	void Run(int dir) {
+		run(dir);
+	}
+	bool Collision() {
+		return collision();
+	}
+	void Faster() {
+		faster();
+	}
+	bool Eat(int x,int y) {
+		return eat(x, y);
+	}
+};
+class Food
+{
+private:
+	int x;//横坐标
+	int y;//纵坐标
+	void setxy(Snack& snack);
+public:
+	Food() = default;
+	Food(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	void Setxy(Snack& snack) {
+		setxy(snack);
+	}
+	Coor getxy() {
+		return Coor({ this->x,this->y });
+	}
+};
+
+
+
